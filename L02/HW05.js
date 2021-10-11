83f3e86419e1753357e8465d9cd74b526bad248c
PMDimitrova <palmina.dimitrova@gmail.com>
2021-10-11 20:29:29 +0300

    HW added task 5

--- L02/HW05.js
+++ L02/HW05.js
@@ -2,8 +2,26 @@ let first = 1;
 let second = 12;
 let third = 123;
 
-let temp = NaN;
-
-if (first > second){
-
+if ((first >= second) && (first >= third)) {
+    if (second >= third) {
+        console.log(first + " " + second + " " + third);
+    } else {
+        console.log(first + " " + third + " " + second);
+    }
+} else {
+    if ((second >= first) && (second >= third)) {
+        if (first >= third) {
+            console.log(second + " " + first + " " + third);
+        } else {
+            console.log(second + " " + third + " " + first);
+        }
+    } else {
+        if (second >= first) {
+            console.log(third + " " + second + " " + first);
+        } else {
+            console.log(third + " " + first + " " + second);
+        }
+    }
 }
+
+//source : stackoverflow; couldn't think of a solution myself
