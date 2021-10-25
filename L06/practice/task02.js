b6b979b06da47b0172846ab59010855974781598
PMDimitrova <palmina.dimitrova@gmail.com>
2021-10-25 12:58:07 +0300

    correction for a variable

--- L06/practice/task02.js
+++ L06/practice/task02.js
@@ -7,7 +7,7 @@ for (let i = 1; i < table.length - 1 ; i++) {
     let previousNum = table[i - 1];
     let nextNum = table[i + 1];
 
-    if (!(((currentNum < previousNum) && (currentNum < nextNum)) || ((currentNum> previousNum) && (currentNum > previousNum)))){
+    if (!(((currentNum < previousNum) && (currentNum < nextNum)) || ((currentNum> previousNum) && (currentNum > nextNum)))){
         isNazyben = false;
         break;
     }
